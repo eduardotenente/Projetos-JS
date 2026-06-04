c97df1b84950920bde01d2d7afeff443bf90c6b5
Eduardo Tenente <eduardodstenente@gmail.com>
2026-06-04 19:54:03 -0300

    Controle de Estoque

--- Lista-GhatGPT/Médio/Controle_de_Estoque/Estoque.js
+++ Lista-GhatGPT/Médio/Controle_de_Estoque/Estoque.js
@@ -1,23 +1,42 @@
 /*
-    Controle de Estoque
+    --- Controle de Estoque ---
 
-    Desenvolva um sistema que:
-    receba nome do produto
-    quantidade em estoque
-    quantidade vendida
-
-    Calcule:
-    estoque restante
-
-    Informe também se o estoque está:
-    normal
-    baixo
-    zerado
-    */
+        Sistema que:
+        Recebe nome do produto.
+        Mostra: quantidade em estoque e quantidade vendida.
+        Calcula o estoque restante e informa.
+        Informa a condição do estoque: zerado, baixo, normal ou alto.
+*/
 
 console.log('-------------------------')
 console.log(' Empresa JA Suplementos')
-console.log(' *Estoque da Empresa*')
+console.log('  *Estoque da Empresa*')
 console.log('-------------------------')
 
-let nome = 
+const produtos = ['Creatina', 'Whey Protein', 'Pré-treino', 'Barra Fitness']
+const estoqueTotal = Math.floor(Math.random() * 500 + 500)
+const vendidos = Math.floor(Math.random() * 900 + 1)
+let estoqueRestante = estoqueTotal - vendidos
+let condiçãoEstoque = ``
+if(estoqueRestante < 0) {
+    estoqueRestante = 0
+}
+
+function sortearProduto(listaDeProdutos) {
+    const indiceAleatorio = Math.floor(Math.random() * listaDeProdutos.length)
+    return listaDeProdutos[indiceAleatorio]
+}
+console.log(`Produto requisitado: ${sortearProduto(produtos)}`)
+console.log(`- Quantidade em estoque: ${estoqueTotal}`)
+console.log(`- Quantidade vendida: ${vendidos}`)
+console.log(`- Estoque restante: ${estoqueRestante}`)
+if(estoqueRestante == 0) {
+    condiçãoEstoque = `Zerado`
+} else if(estoqueRestante > 0 && estoqueRestante < 300) {
+    condiçãoEstoque = `Baixo`
+} else if(estoqueRestante > 300 && estoqueRestante < 600) { 
+    condiçãoEstoque = `Normal`
+} else if(estoqueRestante > 600) {
+    condiçãoEstoque = `Alto`
+}
+console.log(`- Condição do Estoque = ${condiçãoEstoque}`)
