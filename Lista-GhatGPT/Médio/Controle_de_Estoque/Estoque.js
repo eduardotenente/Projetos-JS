5ee7678a1adda6ed1a8e887acbb6d06093856ee4
Eduardo Tenente <eduardodstenente@gmail.com>
2026-06-04 19:00:51 -0300

    Criando um Controle de Estoque

---
+++ Lista-GhatGPT/Médio/Controle_de_Estoque/Estoque.js
@@ -0,0 +1,23 @@
+/*
+    Controle de Estoque
+
+    Desenvolva um sistema que:
+    receba nome do produto
+    quantidade em estoque
+    quantidade vendida
+
+    Calcule:
+    estoque restante
+
+    Informe também se o estoque está:
+    normal
+    baixo
+    zerado
+    */
+
+console.log('-------------------------')
+console.log(' Empresa JA Suplementos')
+console.log(' *Estoque da Empresa*')
+console.log('-------------------------')
+
+let nome = 
